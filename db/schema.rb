bdf30aac6c9909263cf595563f8dde67eefb2242
Nivea Vilar <niveavilar@gmail.com>
2023-11-21 12:09:19 +0100

    updated schema with image_url and updated the seed file with images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_20_162913) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_21_103858) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_20_162913) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "image_url"
   end
 
   create_table "users", force: :cascade do |t|
