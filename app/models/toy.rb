32a29666174860cd9359d4cd44cd5a5e009967d7
Riru Mizutani <seahorse3106@gmail.com>
2023-11-23 02:28:12 +0900

    conflict bug fixed

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -1,4 +1,6 @@
 class Toy < ApplicationRecord
   belongs_to :user
   validates :name, :price, :description, :age, :image_url, :location, presence: true
+  geocoded_by :location
+  after_validation :geocode
 end
---
+++ db/migrate/20231122172139_remove_flats.rb
@@ -0,0 +1,5 @@
+class RemoveFlats < ActiveRecord::Migration[7.1]
+  def change
+    drop_table :flats
+  end
+end
