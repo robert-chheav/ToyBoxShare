ced588382dc1f0828dec68efcd515a4c20162acc
Rita Goncalves <ritamendesgoncalves@gmail.com>
2023-11-22 12:15:03 +0100

    Add Logic for delete method

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   resources :users do
     resources :reservations
   end
+
+  resources :reservations, only: :destroy
   # Defines the root path route ("/")
   # root "posts#index"
 end
