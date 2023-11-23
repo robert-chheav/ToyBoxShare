df6f64b54bf93c58575a878edd47ca5a26ff3b2b
Rita Goncalves <ritamendesgoncalves@gmail.com>
2023-11-23 14:28:43 +0100

    Add Logic method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,6 @@ class PagesController < ApplicationController
   def dashboard
     @reservations = current_user.reservations
     @toys = Toy.where(user_id: current_user)
+    @toy = Toy.new
   end
 end
